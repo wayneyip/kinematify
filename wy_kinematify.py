50119a5729a897ba168b341461c65e6a0fff041a
Wayne Yip <yipw@usc.edu>
2019-11-14 22:36:02 -0800

    Modify algorithm to account for extra child joints at start and end

--- wy_kinematify.py
+++ wy_kinematify.py
@@ -1,59 +1,59 @@
 import maya.cmds as cmds
-import re
 
 def kinematify(mainChainStartJoint, mainChainEndJoint, ikControl, ikfkSwitch, jointNameSuffix):
 
-	numLayers = getLayersBetweenJoints(startJoint, endJoint)
+	mainChain = getJointChainBetween(mainChainStartJoint, mainChainEndJoint, [])
+	numLayers = len(mainChain)
 
 	# Create IK chain
-	ikChain = cmds.duplicate(mainChainStartJoint, renameChildren=1)
+	cmds.select(clear=1)
+	ikChain = []
+	for joint in mainChain:
+		jointPos = cmds.xform(joint, q=1, rotatePivot=1, worldSpace=1)
+		ikJoint = cmds.joint(
+			position=jointPos,
+			name=joint.replace(jointNameSuffix, '_IK'+jointNameSuffix),
+			radius=cmds.joint(joint, q=1, radius=1)[0]
+		)
+		ikChain.append(ikJoint)
 
-	# ...Rename duplicated IK joints 
-	for i, ikJoint in enumerate(ikChain):
-		if cmds.objectType(ikJoint) == 'joint':
-			ikJoint = cmds.rename(ikJoint, re.sub('{}.*'.format(jointNameSuffix), '_IK{}'.format(jointNameSuffix), ikJoint))
-			ikChain.pop(i)
-			ikChain.insert(i, ikJoint)
-
-	# ...Remove extra duplicated joints at the end
 	ikChainStartJoint = ikChain[0]
-	ikChainEndJoint = getChildJointAtLayer(ikChainStartJoint, numLayers)
-	ikChainEndJointChildren = cmds.listRelatives(ikChainEndJoint, children=1)
-	if ikChainEndJointChildren is not None:
-		cmds.delete(ikChainEndJointChildren)
-	
+	ikChainEndJoint = ikChain[len(ikChain)-1]
+
 	# ...Create IK handle
 	ikHandle = cmds.ikHandle(startJoint=ikChainStartJoint, endEffector=ikChainEndJoint)[0]
 	ikHandle = cmds.rename(ikHandle, ikControl.replace('_CTRL', '_IK'))
 	cmds.parent(ikHandle, ikControl)
 
 	# Create FK chain
-	fkChain = cmds.duplicate(mainChainStartJoint, renameChildren=1)
+	cmds.select(clear=1)
+	fkChain = []
+	for joint in mainChain:
+		jointPos = cmds.xform(joint, q=1, rotatePivot=1, worldSpace=1)
+		fkJoint = cmds.joint(
+			position=jointPos,
+			name=joint.replace(jointNameSuffix, '_FK'+jointNameSuffix),
+			radius=cmds.joint(joint, q=1, radius=1)[0]
+		)
+		fkChain.append(fkJoint)
 
-	# ...Rename duplicated FK joints 
-	for i, fkJoint in enumerate(fkChain):
-		if cmds.objectType(fkJoint) == 'joint':
-			fkJoint = cmds.rename(fkJoint, re.sub('{}.*'.format(jointNameSuffix), '_FK{}'.format(jointNameSuffix), fkJoint))
-			fkChain.pop(i)
-			fkChain.insert(i, fkJoint)
-
-	# ...Remove extra duplicated joints at the end
 	fkChainStartJoint = fkChain[0]
-	fkChainEndJoint = getChildJointAtLayer(fkChainStartJoint, numLayers)
-	fkChainEndJointChildren = cmds.listRelatives(fkChainEndJoint, children=1)
-	if fkChainEndJointChildren is not None:
-		cmds.delete(fkChainEndJointChildren)
+	fkChainEndJoint = fkChain[len(ikChain)-1]
 		
 	# Orient main chain to IK and FK chains
-	orientAll(mainChainStartJoint, ikChainStartJoint, fkChainStartJoint)
+	orientConstrs = []
+	for i in range(len(mainChain)):
+		orientConstr = cmds.orientConstraint(ikChain[i], fkChain[i], mainChain[i], maintainOffset=1)[0]
+		cmds.setAttr(orientConstr+'.interpType', 2) # Shortest interpolation
+		orientConstrs.append(orientConstr)
+
+	cmds.orientConstraint(ikControl, ikChainEndJoint, maintainOffset=1)
 
 	# Connect IKFK switch control to all orient constraints' attributes
 	cmds.addAttr(ikfkSwitch, ln='IK', at='float', min=0, max=1, keyable=1)
 	cmds.addAttr(ikfkSwitch, ln='FK', at='float', min=0, max=1, keyable=1)
 
-	testNode = mainChainEndJoint
-	for i in range(numLayers + 1):
-		orientConstr = cmds.listConnections(testNode, type='orientConstraint')[0]
+	for orientConstr in orientConstrs:
 		attrs = cmds.listAttr(orientConstr, keyable=1)
 		for attr in attrs:
 			for fkJoint in fkChain:
@@ -68,9 +68,6 @@ def kinematify(mainChainStartJoint, mainChainEndJoint, ikControl, ikfkSwitch, jo
 					ikfkSwitchIK = ikfkSwitch + '.IK'
 					if not cmds.isConnected(ikfkSwitchIK, orientConstrAttr):
 						cmds.connectAttr(ikfkSwitchIK, orientConstrAttr)
-
-		if i < numLayers:
-			testNode = cmds.listRelatives(testNode, parent=1)[0]
 
 	# Relate IK attribute to FK attribute		
 	cmds.expression(string="%s = 1 - %s" % (ikfkSwitch + '.FK', ikfkSwitch + '.IK'))
@@ -90,8 +87,11 @@ def kinematify(mainChainStartJoint, mainChainEndJoint, ikControl, ikfkSwitch, jo
 				parentCtrl = parentCtrls[0]
 				cmds.parentConstraint(parentCtrl, fkControl, maintainOffset=1)
 
-	# Point constrain IK start to main chain start:
+	# Point constrain IK start to main chain start
 	cmds.pointConstraint(mainChainStartJoint, ikChainStartJoint)
+
+	# Parent constraint IKFK switch to end joint
+	cmds.parentConstraint(mainChainEndJoint, ikfkSwitch, maintainOffset=1)
 
 	# Lock IK/FK switch attributes
 	cmds.setAttr(ikfkSwitch+'.tx', keyable=0)
@@ -123,39 +123,24 @@ def kinematify(mainChainStartJoint, mainChainEndJoint, ikControl, ikfkSwitch, jo
 	cmds.parent(ikfkGrp, worldGroup)
 
 
-def getLayersBetweenJoints(parentJoint, childJoint):
+def getJointChainBetween(startJoint, endJoint, jointChain):
 	
-	numLayers = 0
-	joint = childJoint
-	while joint != parentJoint:
+	# Ensure that a joint chain exists between start and end joint
+	startJointDescendants = cmds.listRelatives(startJoint, allDescendents=1)
+	if startJointDescendants is None or endJoint not in startJointDescendants:
+		cmds.error('No joint chain found between start and end joints')
+		return
+	
+	joint = endJoint
+	jointChain.append(joint)
+
+	while joint != startJoint:
 		joint = cmds.listRelatives(joint, parent=1)[0]
-		numLayers += 1
-	return numLayers
-	
+		jointChain.append(joint)
 
-def getChildJointAtLayer(parentJoint, layer):
+	jointChain.reverse()
+	return jointChain
 	
-	joint = parentJoint
-
-	while layer > 0:
-		children = cmds.listRelatives(joint, children=1)
-		joint = [child for child in children if cmds.objectType(child) == 'joint'][0]
-		layer -= 1
-
-	return joint
-	
-
-def orientAll(mainChainJoint, ikChainJoint, fkChainJoint):
-	
-	cmds.orientConstraint(ikChainJoint, fkChainJoint, mainChainJoint, maintainOffset=0)
-	
-	mainChildren = cmds.listRelatives(mainChainJoint, children=1)
-	ikChildren = cmds.listRelatives(ikChainJoint, children=1)
-	fkChildren = cmds.listRelatives(fkChainJoint, children=1)
-	
-	if mainChildren is not None and ikChildren is not None and fkChildren is not None:
-		orientAll(mainChildren[0], ikChildren[0], fkChildren[0])
-
 
 def getParentJoint(jnt):
 
@@ -172,44 +157,45 @@ def getParentJoint(jnt):
 
 def createFkControls(fkJoint, jointNameSuffix):
 
+	fkControl = createControlSphere(0.5)
+	fkControl = cmds.rename(fkControl, fkJoint.replace(jointNameSuffix, '_CTRL'))
+
+	# Color handling
+	if fkControl.startswith('LF'):
+		cmds.color(fkControl, rgb=(0,0,1))
+	elif fkControl.startswith('RT'):
+		cmds.color(fkControl, rgb=(1,0,0))
+	
+	# Lock translation and scale
+	cmds.setAttr(fkControl+'.tx', lock=1, keyable=0)
+	cmds.setAttr(fkControl+'.ty', lock=1, keyable=0)
+	cmds.setAttr(fkControl+'.tz', lock=1, keyable=0)
+	cmds.setAttr(fkControl+'.sx', lock=1, keyable=0)
+	cmds.setAttr(fkControl+'.sy', lock=1, keyable=0)
+	cmds.setAttr(fkControl+'.sz', lock=1, keyable=0)
+
+	# Create group node
+	grp = cmds.group(fkControl, name=fkControl.replace('_CTRL', '_GRP'))
+	cmds.matchTransform(grp, fkJoint)
+	
+	# Constrain joint to control
+	cmds.parentConstraint(fkControl, fkJoint, maintainOffset=1)
+
 	fkChildren = cmds.listRelatives(fkJoint, children=1)
 	if fkChildren is not None:
+
 		numChildJoints = 0
+
 		for fkChild in fkChildren:
 			if cmds.objectType(fkChild) == 'joint':
 				numChildJoints += 1
+
 		if numChildJoints == 1: 
-			fkControl = createControlSphere(1)
-			fkControl = cmds.rename(fkControl, fkJoint.replace(jointNameSuffix, '_CTRL'))
-
-			# Color handling
-			if fkControl.startswith('LF'):
-				cmds.color(fkControl, rgb=(0,0,1))
-			elif fkControl.startswith('RT'):
-				cmds.color(fkControl, rgb=(1,0,0))
-			
-			# Lock translation and scale
-			cmds.setAttr(fkControl+'.tx', lock=1, keyable=0)
-			cmds.setAttr(fkControl+'.ty', lock=1, keyable=0)
-			cmds.setAttr(fkControl+'.tz', lock=1, keyable=0)
-			cmds.setAttr(fkControl+'.sx', lock=1, keyable=0)
-			cmds.setAttr(fkControl+'.sy', lock=1, keyable=0)
-			cmds.setAttr(fkControl+'.sz', lock=1, keyable=0)
-
-			# Create group node
-			grp = cmds.group(fkControl, name=fkControl.replace('_CTRL', '_GRP'))
-			cmds.matchTransform(grp, fkJoint)
-			
-			# Constrain joint to control
-			cmds.parentConstraint(fkControl, fkJoint, maintainOffset=1)
-			
 			fkChildGrp = createFkControls(fkChildren[0], jointNameSuffix)
 			if fkChildGrp is not None:
 				cmds.parent(fkChildGrp, fkControl)
 
-			return grp
-
-	return None
+	return grp
 
 
 def createControlSphere(radius):
